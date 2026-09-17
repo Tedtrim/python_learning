4babbef94087018a09832bb56e23264882806972
Tedtrim <ebt22@sussex.ac.uk>
2026-09-17 10:16:57 +0100

    Organised DSA into folders

--- DSA/LinkedList/Leetcode19.py
+++ DSA/LinkedList/Leetcode19.py
@@ -1,3 +1,8 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
 def removeNthFromEnd(self, head, n):
     current = head
     size = 0
